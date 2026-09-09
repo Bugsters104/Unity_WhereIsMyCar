06d79e095e0af1bda45db53abce84469b36272b1
Bugsters104 <pasts@pasts.lv>
2026-09-09 14:37:07 +0300

    Created first script: GameObjectScript

---
+++ Assets/Scripts/GameObjectsScript.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class GameObjectsScript : MonoBehaviour
+{
+    public GameObject garbageTruck;
+    public GameObject medicine;
+    public GameObject schoolBuss;
+
+    [HideInInspector] 
+    public Vector2 garbageTruckCoord;
+    [HideInInspector]
+    public Vector2 medicineCoord;
+    [HideInInspector]
+    public Vector2 schoolBussCoord;
+
+    public Canvas canvas;
+    public AudioSource carSoundSource;
+    public AudioClip[] sounds;
+
+    [HideInInspector]
+    public bool inRightPlace = false;
+    public static GameObjectsScript lastDragged = null;
+    public static bool isDragging = false;
+
+
+    
+    void Awake()
+    {
+        garbageTruckCoord = garbageTruck.GetComponent<RectTransform>().localPosition;
+        medicineCoord = medicine.GetComponent<RectTransform>().localPosition;
+        schoolBussCoord = schoolBuss.GetComponent<RectTransform>().localPosition;
+    }
+}
