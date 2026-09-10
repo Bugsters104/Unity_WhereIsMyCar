5362db4da68fd715ad2b1ba5acf4f3d90a636349
Bugsters104 <pasts@pasts.lv>
2026-09-10 13:27:34 +0300

    Start ScreenBoundriesScript

--- Assets/Scripts/DragAndDropScript.cs
+++ Assets/Scripts/DragAndDropScript.cs
@@ -31,7 +31,17 @@ public class DragAndDropScript : MonoBehaviour,
     }
     public void OnBeginDrag(PointerEventData eventData)
     {
-       
+       if(Input.GetMouseButton(0) && !Input.GetMouseButton(1) && !Input.GetMouseButton(2))
+        {
+           GameObjectsScript.isDragging = true;
+            canvasGroup.alpha = 0.6f;
+            canvasGroup.blocksRaycasts = false;
+            int lastIndex = transform.parent.childCount - 1;
+            int positionIndex = Mathf.Max(0, lastIndex - 1);
+            transform.SetSiblingIndex(positionIndex);
+
+            // Izstrādāsim ScreenBoudnries un tad varēs taisīt pārvietošanu...
+        }
     }
 
     public void OnDrag(PointerEventData eventData)
@@ -41,7 +51,21 @@ public class DragAndDropScript : MonoBehaviour,
 
     public void OnEndDrag(PointerEventData eventData)
     {
-      
+      if(Input.GetMouseButton(0))
+        {
+            GameObjectsScript.isDragging = false;
+            Debug.Log("OnEndDrag called for " + gameObject.name);
+            canvasGroup.alpha = 1f;
+            canvasGroup.blocksRaycasts = true;
+
+            if(gameObjectsScript.inRightPlace)
+            {
+                canvasGroup.blocksRaycasts = false;
+                GameObjectsScript.lastDragged = null;
+            }
+
+            gameObjectsScript.inRightPlace = false;
+        }
     }
 
    
